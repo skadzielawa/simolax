c4dbb06b1bed1f0e3d3e77481c6449ab614e4cb3
awesem-szymon <szymon@awesem.com>
2022-12-16 12:17:56 +0100

    prettier added, code formatted

--- simolax.js
+++ simolax.js
@@ -1,66 +1,82 @@
 /* Simolax v1.0.0 */
-(function($){
-	"use strict";
+(function ($) {
+	'use strict';
 
-	$.fn.simolax = function( options ) {
-
+	$.fn.simolax = function (options) {
 		// default options
 		// var settings = $.extend({
 		// 	ratio: '0.5'
 		// }, options);
 
 		// function to check if element is in viewport
-		function isElementInViewport (el) {
-	    // special bonus for those using jQuery
-	    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-	    }
-	    var rect = el.getBoundingClientRect();
-	  	return (
+		function isElementInViewport(el) {
+			// special bonus for those using jQuery
+			if (typeof jQuery === 'function' && el instanceof jQuery) {
+				el = el[0];
+			}
+			var rect = el.getBoundingClientRect();
+			return (
 				rect.bottom >= 0 &&
 				rect.right >= 0 &&
-				rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-				rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+				rect.top <=
+					(window.innerHeight ||
+						document.documentElement.clientHeight) &&
+				rect.left <=
+					(window.innerWidth || document.documentElement.clientWidth)
 			);
 		}
 
 		function setTransformation(el, img, ratio, isCentered) {
-			if (typeof jQuery === "function" && el instanceof jQuery && img instanceof jQuery) {
-        el = el[0];
+			if (
+				typeof jQuery === 'function' &&
+				el instanceof jQuery &&
+				img instanceof jQuery
+			) {
+				el = el[0];
 				img = img[0];
-	    }
+			}
 			var rect = el.getBoundingClientRect();
 			var top = rect.top;
 			var height = rect.height;
 
 			// set CSS transform
-			if(isCentered) {
+			if (isCentered) {
 				var width = img.clientWidth;
-				$(img).css('transform', 'translate3D(' + (-width/2) + 'px, ' + top * ratio + 'px, 0)' );
+				$(img).css(
+					'transform',
+					'translate3D(' +
+						-width / 2 +
+						'px, ' +
+						top * ratio +
+						'px, 0)'
+				);
 			} else {
-				$(img).css('transform', 'translate3D( 0px, ' + top * ratio + 'px, 0)' );
+				$(img).css(
+					'transform',
+					'translate3D( 0px, ' + top * ratio + 'px, 0)'
+				);
 			}
 		}
 
 		var element = $('[data-simolax-section]');
 
-		element.each(function(){
+		element.each(function () {
 			var $this = $(this);
 			var $image = $(this).find('[data-simolax-item]');
 			var ratio = $image.data('simolax-ratio');
 			var isCentered = $image.data('simolax-iscentered');
 
-			setTransformation( $this, $image, ratio, isCentered);
+			setTransformation($this, $image, ratio, isCentered);
 
-			$(window).on('resize', function(){
-				setTransformation( $this, $image, ratio, isCentered);
+			$(window).on('resize', function () {
+				setTransformation($this, $image, ratio, isCentered);
 			});
 
-			$(window).on('scroll', function(){
-				if( isElementInViewport( $this ) ) {
-					setTransformation( $this, $image, ratio, isCentered);
+			$(window).on('scroll', function () {
+				if (isElementInViewport($this)) {
+					setTransformation($this, $image, ratio, isCentered);
 				}
 			});
 		});
-	}
+	};
 })(jQuery);
